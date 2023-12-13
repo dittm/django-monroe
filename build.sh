c3afa136924bd3828feedda069dfdf67f78f972e
Christina Dittmann <dittmann.christina@gmx.de>
2023-12-13 15:00:04 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Build the project
+echo "Building the project"
+python -m pip install -r requirements.txt
+
+echo "Make migration"
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collect static"
+python manage.py collectstatic --noinput --clear
+
